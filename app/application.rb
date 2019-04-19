abeebec18dfe27657533c04d0c8fb9e056bcee56
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-19 17:40:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,11 +7,11 @@ class Application
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
 
-    resp.write("#{num_1}, #{num_2}, #{num_3}\n")
+    resp.write "#{num_1}, #{num_2}, #{num_3}\n"
     if num_1 == num_2 && num_2 == num_3
-      resp.write("You Win!")
+      resp.write "You Win!"
     else
-      resp.write("You Lose!")
+      resp.write "You Lose!"
     end
 
     resp.finish
